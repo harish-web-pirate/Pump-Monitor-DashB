beaa4c96361965214e6e3f7aea2d6739e0bc205b
Monigasri <monigasri07@gmail.com>
2025-03-07 11:26:11 +0530

    A new branch created by Monigasri

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Login from './components/login_page/login';
+export default function App() {
+  return (
+    <>
+    <Login/>
+    <p> branch created by monigasri</p>
+    </>
+  )
+}
