ec081884a6f7cca3a3711fa389322f5a7cd802d9
Arun Bhagavath <mhariharan1003@gmail.com>
2025-03-19 15:43:26 +0530

    gauge

--- backend/database.php
+++ backend/database.php
@@ -8,7 +8,5 @@ $conn = mysqli_connect($db_server, $db_username, $db_password, $db_database);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-else{
-    echo "Connected successfully";
-}
+
 ?>
--- backend/index.php
+++ backend/index.php
@@ -1,13 +1,41 @@
 <?php
 include('database.php');
 
-$query = "SELECT * FROM pump_power_monitoring";
-$result = mysqli_query($conn, $query);  
-if(mysqli_num_rows($result)>0){
-    $fetched_data = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    echo "Pump Power Monitoring Data: <br>";
-    foreach($fetched_data as $data){
-        echo "ID:" .$data['id']. "Running_data".$data['running_frequency']."<br>";
-    }
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Cache-Control: no-cache, no-store, must-revalidate");
+header("Expires: 0");
+
+
+$data = json_decode(file_get_contents("php://input"), true);
+if (empty($data['username']) || empty($data['password'])) {
+    echo json_encode(["success" => false, "message" => "Empty Field!"]);
+    exit;
 }
+
+if (isset($data['username']) && isset($data['password'])) {
+    $username = mysqli_real_escape_string($conn, $data['username']);
+    $password = mysqli_real_escape_string($conn, $data['password']);
+
+    $query = "SELECT * FROM login_credentials WHERE user_name = 'admin'";
+    $result = mysqli_query($conn, $query);
+
+    if (mysqli_num_rows($result) > 0) {
+        $user = mysqli_fetch_assoc($result);
+
+        
+        if ($password === "admin123") {
+            echo json_encode(["success" => true, "message" => "Login successful"]);
+        } else {
+            echo json_encode(["success" => false, "message" => "Invalid password"]);
+        }
+    } else {
+        echo json_encode(["success" => false, "message" => "User not found"]);
+    }
+} else {
+    echo json_encode(["success" => false, "message" => "Invalid request"]);
+}
+
+mysqli_close($conn);
 ?>
