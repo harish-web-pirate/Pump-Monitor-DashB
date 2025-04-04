b2d760e3914aaf905445f7e18c4fd1e8f269ecca
Kavi Prasath K <166359438+Kaviprasathk47@users.noreply.github.com>
2025-04-04 16:11:47 +0530

    Updated database.php

--- backend/database.php
+++ backend/database.php
@@ -3,7 +3,7 @@ $db_server = "localhost";
 $db_username = "root";  
 $db_password = "";
 $db_database = "pump_power";
-$db_name = "pump_power_monitoring";
+//$db_name = "pump_power_monitoring";
 $conn = mysqli_connect($db_server, $db_username, $db_password, $db_database);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
